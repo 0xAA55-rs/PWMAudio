e8667d7ab85212904ca0b4bb041b8aff2449316f
0xAA55 <weixiaobao213@qq.com>
2025-05-24 17:47:43 +0800

    它 USB 那边还有个“静音”的命令，这边设置一个静音缓冲区，也就是静音的时候用 PWM 播放这个缓冲区来实现静音。

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -52,6 +52,9 @@ uint16_t pwm_ch1_buffer[BUFFER_SIZE];
 uint16_t pwm_ch2_buffer[BUFFER_SIZE];
 uint16_t* pwm_ch1_buffer_half = &pwm_ch1_buffer[BUFFER_SIZE / 2];
 uint16_t* pwm_ch2_buffer_half = &pwm_ch2_buffer[BUFFER_SIZE / 2];
+uint16_t pwm_mute_buffer[MUTE_BUFFER_SIZE];
+const uint16_t mute_val = 750;
+int is_muted = 0;
 uint32_t Volume_Modifier = 100;
 /* USER CODE END PV */
 
