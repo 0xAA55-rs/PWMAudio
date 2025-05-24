f495d1cbb7cb2e91d029c79abc43b9c3b8bf8ca1
0xAA55 <weixiaobao213@qq.com>
2025-05-24 17:28:29 +0800

    因为要整 USB，当然要把 USB 的头文件拉进来啦

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "usbd_audio.h"
+#include "usbd_audio_if.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
