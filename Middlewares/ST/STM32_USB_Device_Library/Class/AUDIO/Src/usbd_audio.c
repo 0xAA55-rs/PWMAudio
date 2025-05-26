f4b3545990c66cabd3c5e7a74605ce4c32d1d4b4
0xAA55 <weixiaobao213@qq.com>
2025-05-26 15:40:42 +0800

    所有收包的地方都发送一个空包作为回应。

--- Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/usbd_audio.c
+++ Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/usbd_audio.c
@@ -561,21 +561,20 @@ static uint8_t  USBD_AUDIO_EP0_RxReady(USBD_HandleTypeDef *pdev)
   switch(haudio->control.cmd)
   {
     case AUDIO_REQ_SET_CUR:
-      USBD_CtlSendStatus(pdev);
       switch (haudio->control.feature_control)
       {
         case AUDIO_CONTROL_MUTE:
           return userdata->MuteCtl(haudio->control.data[0]);
         case AUDIO_CONTROL_VOLUME:
           return userdata->VolumeCtl(haudio->control.channel, haudio->control.data[0]);
-        default:
-          return USBD_FAIL;
       }
+      USBD_CtlSendStatus(pdev);
       return USBD_OK;
     case AUDIO_REQ_SET_MIN:
     case AUDIO_REQ_SET_MAX:
     case AUDIO_REQ_SET_RES:
-      return USBD_FAIL;
+      USBD_CtlSendStatus(pdev);
+      return USBD_OK;
     case AUDIO_REQ_GET_CUR:
       AUDIO_REQ_GetCur(pdev, &pdev->request);
       return USBD_OK;
@@ -675,6 +674,7 @@ static uint8_t  USBD_AUDIO_DataOut(USBD_HandleTypeDef *pdev, uint8_t epnum)
 
   if (epnum == AUDIO_OUT_EP)
   {
+    USBD_CtlSendStatus(pdev);
     haudio->wr_ptr += AUDIO_OUT_PACKET;
     if (haudio->wr_ptr == AUDIO_HALF_BUF_SIZE)
     {
